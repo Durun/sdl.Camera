cd28840e56c56216347b8544ca897eb2166c10ca
Durun <negiigainuki@gmail.com>
2020-05-22 17:27:56 +0900

    capture photo

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.kt
@@ -1,27 +1,28 @@
 package jp.ac.titech.itpro.sdl.camera
 
 import android.app.Activity
+import android.content.ComponentName
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val photoImage: Bitmap? = null
+    private var photoImage: Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         photo_button.setOnClickListener {
-            val intent = Intent()
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             // TODO: You should setup appropriate parameters for the intent
-            val activities: List<*> = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
-            if (activities.isNotEmpty()) {
+            val activity: ComponentName? = intent.resolveActivity(packageManager)
+            activity?.let {
                 startActivityForResult(intent, REQ_PHOTO)
-            } else {
+            } ?: run {
                 Toast.makeText(this@MainActivity, R.string.toast_no_activities, Toast.LENGTH_LONG).show()
             }
         }
@@ -40,6 +41,9 @@ class MainActivity : AppCompatActivity() {
         if (reqCode == REQ_PHOTO) {
             if (resCode == Activity.RESULT_OK) {
                 // TODO: You should implement the code that retrieve a bitmap image
+                photoImage = data?.let {
+                    it.extras?.get("data") as Bitmap
+                }
             }
         }
     }
