ea8a7507747a3ba3ebbf64b89569ae9749e1115e
Durun <negiigainuki@gmail.com>
2020-05-23 02:56:12 +0900

    fix

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.kt
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.ComponentName
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
@@ -15,7 +14,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
-import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private var photoImage: Bitmap? = null
@@ -27,14 +25,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        loadPhoto()
+
         photo_button.setOnClickListener { takePhoto() }
     }
 
     private fun takePhoto() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         // TODO: You should setup appropriate parameters for the intent
-        val photoUri = createPhotoUri()
+        val photoUri = FileProvider.getUriForFile(this, FILEPROVIDER_AUTHORITY, photoFile)
         val activity: ComponentName? = intent.resolveActivity(packageManager)
+
         activity?.let {
             intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
             startActivityForResult(intent, REQ_PHOTO)
@@ -43,17 +44,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun loadPhoto() {
+        photoImage = photoFile.decodeForView(photo_view)
+    }
+
     private fun showPhoto() {
-        photoImage?.let {
-            photo_view.setImageBitmap(photoImage)
-        }
+        photoImage?.let { photo_view.setImageBitmap(it) }
     }
 
     override fun onActivityResult(reqCode: Int, resCode: Int, data: Intent?) {
         super.onActivityResult(reqCode, resCode, data)
         if ((reqCode == REQ_PHOTO) && (resCode == Activity.RESULT_OK)) {
             // TODO: You should implement the code that retrieve a bitmap image
-            photoImage = photoFile.decodeForView(photo_view)
+            loadPhoto()
         }
     }
 
@@ -62,30 +65,10 @@ class MainActivity : AppCompatActivity() {
         showPhoto()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-
-        photoFile.takeIf { it.exists() }?.delete()  // delete photo file on exit
-    }
-
-    private fun File.decodeForView(view: ImageView): Bitmap {
-        val option = BitmapFactory.Options().apply {
-            val targetW = view.width
-            val targetH = view.height
-            val photoW = outWidth
-            val photoH = outHeight
-
-            val scaleFactor: Int = (photoW / targetW).coerceAtMost(photoH / targetH)
-            inJustDecodeBounds = false
-            inSampleSize = scaleFactor
+    private fun File.decodeForView(view: ImageView): Bitmap? {
+        return this.takeIf { this.exists() }?.let {
+            BitmapFactory.decodeFile(it.path)
         }
-        return BitmapFactory.decodeFile(this.path, option)
-    }
-
-    @Throws(IOException::class)
-    private fun createPhotoUri(): Uri {
-        photoFile.takeUnless { it.exists() }?.createNewFile()
-        return FileProvider.getUriForFile(this, FILEPROVIDER_AUTHORITY, photoFile)
     }
 
     companion object {
